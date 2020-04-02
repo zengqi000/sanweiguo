b217fe8b6ef7319aa79fc3bddc80808464b358b2
zengqi000 <787466095@qq.com>
2020-04-02 11:06:06 +0800

    优化

--- Interface_TestPlatform-master/apps/apitest/guo.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?"
-parameter_dict = {"tel":17764591649}
-r = requests.get(url, headers={}, params=parameter_dict)
-print(r.text)
--- Interface_TestPlatform-master/apps/apitest/task.py
+++
@@ -1,7 +0,0 @@
-from Interface_TestPlatform.celery import app
-
-
-@app.task
-def hello_world():
-    a = "sdf"
-    return a
